1ef55bdcb1f2dee4f388773f177c92d1613d5bae
elayneargollo <elayne.natalia@outlook.com>
2021-04-10 19:11:13 -0300

    adicionando o restante das rotas

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ContaRestaurante from '../components/contaRestaurante/ContaRestaurante';
-//import Formulario from '../components/formulario/Formulario';
+import Formulario from '../components/formulario/Formulario';
 import IMC from '../components/imc/IMC';
-//import JogoDaVelha from '../components/jogaDaVelha/JogoDaVelha';
+import JogoDaVelha from '../components/jogaDaVelha/JogoDaVelha';
 import ZeroaCem from '../components/jogoZeroaCem/ZeroaCem';
 import ZeroaCemMelhorado from '../components/jogoZeroaCemMelhorado/ZeroaCemMelhorado';
-//import QuadroMagico from '../components/quadroMagico/QuadroMagico';
+import QuadroMagico from '../components/quadroMagico/QuadroMagico';
 
 function Rotas() {
   
@@ -17,6 +17,9 @@ function Rotas() {
         <Route exact path="/questao2" component={ContaRestaurante} />
         <Route exact path="/questao3" component={ZeroaCem} />
         <Route exact path="/questao4" component={ZeroaCemMelhorado} />
+        <Route exact path="/questao5" component={Formulario} />
+        <Route exact path="/questao6" component={JogoDaVelha} />
+        <Route exact path="/questao7" component={QuadroMagico} />
       </Switch>
     </BrowserRouter>
   );
