9774f60ba89071e0fc5304788a8fc24c0b746b4f
elayneargollo <elayne.natalia@outlook.com>
2021-04-10 12:57:17 -0300

    logica restaurante

--- src/components/contaRestaurante/ContaRestaurante.js
+++ src/components/contaRestaurante/ContaRestaurante.js
@@ -2,10 +2,63 @@ import React, { Component } from 'react';
 
 export default class ContaRestaurante extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      gastoTotalMesa: 0,
+      numeroDePessoasSentadas: 0,
+      porcentualPagoGorjeta: 0,
+      totalGeral: "",
+      totalPorPessoa: "",
+    };
+  }
+
+  emitirNota() {
+    let state = this.state;
+    
+    let totalGeral = Number(state.gastoTotalMesa) + Number(state.gastoTotalMesa * (state.porcentualPagoGorjeta/100));
+    state.totalGeral = totalGeral;
+
+    let totalPorPessoa =  totalGeral/state.numeroDePessoasSentadas;
+    state.totalPorPessoa = totalPorPessoa;
+
+    this.setState(state);
+  }
+
   render() {
     return (
       <div className="restaurante">
       <h1>Restaurante</h1>
+
+      Gasto total da mesa:{" "}
+        <input
+          type="text"
+          name="gastoTotalMesa"
+          value={this.state.gastoTotalMesa}
+          onChange={(e) => this.setState({ gastoTotalMesa: e.target.value })}
+        />
+
+        Número de pessoas sentadas:{" "}
+        <input
+          type="text"
+          name="numeroDePessoasSentadas"
+          value={this.state.numeroDePessoasSentadas}
+          onChange={(e) => this.setState({ numeroDePessoasSentadas: e.target.value })}
+        />
+
+        Porcentagem gorjeta garçom:{" "}
+        <input
+          type="text"
+          name="porcentualPagoGorjeta"
+          value={this.state.porcentualPagoGorjeta}
+          onChange={(e) => this.setState({ porcentualPagoGorjeta: e.target.value })}
+        />
+
+        <button onClick={this.emitirNota.bind(this)}>Emitir Nota</button>
+        <br />
+        <h6>Total a ser pago: R$ {this.state.totalGeral}</h6>
+        <h6>Valor por pessoa: R$ {this.state.totalPorPessoa}</h6>
+
       </div>
     );
   }
--- src/components/imc/IMC.js
+++ src/components/imc/IMC.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class IMC extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peso: 0,
+      altura: 0,
+      imc: "",
+    };
+  }
+
+  calcular() {
+    let state = this.state;
+    let imc = state.peso / state.altura ** 2;
+    state.imc = imc;
+    this.setState(state);
+  }
 
   render() {
     return (
       <div className="imc">
-      <h1>IMC</h1>
+        <h1> IMC </h1>
+        Peso:{" "}
+        <input
+          type="text"
+          name="peso"
+          value={this.state.peso}
+          onChange={(e) => this.setState({ peso: e.target.value })}
+        />
+        Altura:{" "}
+        <input
+          type="text"
+          name="altura"
+          value={this.state.altura}
+          onChange={(e) => this.setState({ altura: e.target.value })}
+        />
+        <button onClick={this.calcular.bind(this)}>Calcular</button>
+        <br />
+        <h1>{this.state.imc}</h1>
       </div>
     );
   }
 }
-
